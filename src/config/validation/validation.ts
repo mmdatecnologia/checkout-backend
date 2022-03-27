941b9610a77fe2ba14fa56a587d6d135703bb739
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2022-03-26 22:22:39 -0300

    chore(config): added missing settings

--- src/config/validation/validation.ts
+++ src/config/validation/validation.ts
@@ -5,5 +5,16 @@ export const validationSchema = Joi.object({
   APP_NAME: Joi.string().default('NestApplication'),
   APP_DESCRIPTION: Joi.string().default('NestApplication'),
   APP_VERSION: Joi.string().required(),
-  PORT: Joi.number().default(3000)
+  PORT: Joi.number().default(3000),
+  CACHE_STORE: Joi.string().valid('redis', 'memory').default('redis'),
+  CACHE_HOST: Joi.string().default('localhost'),
+  CACHE_PASSWORD: Joi.string().default('password'),
+  CACHE_PORT: Joi.number().default(6379),
+  CACHE_PREFIX: Joi.string().default('cache'),
+  DB_TYPE: Joi.string().valid('mongodb').default('mongodb'),
+  DB_USERNAME: Joi.string().default('username'),
+  DB_PASSWORD: Joi.string().default('password'),
+  DB_HOST: Joi.string().default('localhost'),
+  DB_PORT: Joi.number().default(27017),
+  DB_NAME: Joi.string().default('database')
 })

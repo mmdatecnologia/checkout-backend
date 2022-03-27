1d3cd4b5b6d8c8636770b4db18af925d64f2d0f7
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2022-03-27 17:40:47 -0300

    refactor(cache): move config to a factory method

--- src/modules/cache/cache.redis.module.ts
+++ src/modules/cache/cache.redis.module.ts
@@ -1,28 +1,14 @@
+import { redisConfig } from '@checkout/config/factories/redis.config'
 import { CacheModule, Module } from '@nestjs/common'
-import { ConfigModule, ConfigService } from '@nestjs/config'
-import * as redisStore from 'cache-manager-redis-store'
+import { ConfigService } from '@nestjs/config'
 
 import { CacheRedisService } from './cache.redis.service'
 
 @Module({
   imports: [
     CacheModule.registerAsync({
-      imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => {
-        if (configService.get<string>('cache.store') === 'REDIS') {
-          return {
-            store: redisStore,
-            host: configService.get<string>('cache.host'),
-            port: configService.get<string>('cache.port'),
-            ttl: configService.get<number>('cache.ttl'),
-            password: configService.get<string>('cache.password'),
-            prefix: configService.get<string>('cache.prefix')
-          }
-        } else {
-          return null
-        }
-      }
+      useFactory: redisConfig
     })
   ],
   exports: [CacheRedisService],

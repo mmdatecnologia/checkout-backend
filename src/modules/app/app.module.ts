82f907af3453b6cc72e1e8330049ec22e16c47c1
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2022-03-27 01:39:17 -0300

    refactor(app): typeorm config by factory

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,9 +1,9 @@
 import { AppController } from '@checkout/app/app.controller'
 import { AppService } from '@checkout/app/app.service'
 import { configuration } from '@checkout/config/configuration'
+import { typeOrmConfigNoSQL } from '@checkout/config/factories/typeorm.config'
 import { validationSchema } from '@checkout/config/validation/validation'
 import { SessionModule } from '@checkout/session/session.module'
-import { ShoppingEntity } from '@checkout/shopping/entity/shopping.entity'
 import { ShoppingModule } from '@checkout/shopping/shopping.module'
 import { Module } from '@nestjs/common'
 import { ConfigModule, ConfigService } from '@nestjs/config'
@@ -19,19 +19,8 @@ import { TypeOrmModule } from '@nestjs/typeorm'
       validationSchema
     }),
     TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => {
-        console.log('url', configService.get('db.url'))
-        return {
-          type: configService.get('db.type'),
-          url: configService.get('db.url'),
-          entities: [ShoppingEntity],
-          synchronize: true,
-          useNewUrlParser: true,
-          logging: true
-        }
-      }
+      useFactory: typeOrmConfigNoSQL
     }),
     SessionModule,
     ShoppingModule

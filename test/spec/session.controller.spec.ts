357823b6395fc38727722109af26e00c7160a06e
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2022-03-28 00:38:37 -0300

    refactor(test): dto mock factories

--- test/spec/session.controller.spec.ts
+++ test/spec/session.controller.spec.ts
@@ -1,10 +1,8 @@
 import { AuthModule } from '@checkout/auth/auth.module'
-import { AuthService } from '@checkout/auth/auth.service'
 import { CacheRedisModule } from '@checkout/cache/cache.redis.module'
 import { configuration } from '@checkout/config/configuration'
 import { typeOrmConfigNoSQL } from '@checkout/config/factories/typeorm.config'
 import { validationSchema } from '@checkout/config/validation/validation'
-import { SessionDto } from '@checkout/session/DTO/session.dto'
 import { SizeDto } from '@checkout/session/DTO/size.dto'
 import { SessionController } from '@checkout/session/session.controller'
 import { SessionService } from '@checkout/session/session.service'
@@ -14,22 +12,17 @@ import { ConfigModule } from '@nestjs/config'
 import { Test, TestingModule } from '@nestjs/testing'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { MemoryDb } from '@test/mocks/memory-db'
-import { plainToClass } from 'class-transformer'
+import { SessionFactoryDto } from '@test/mocks/session-factory-dto'
 import { validate } from 'class-validator'
-import * as faker from 'faker-br'
 
 jest.mock('@checkout/auth/auth.service')
 
 describe('SessionController', () => {
   let sessionController: SessionController
-  let sessionValue: SessionDto
-
   let app: TestingModule
   const mongod = new MemoryDb()
-
+  const sessionFactoryDto = new SessionFactoryDto()
   beforeAll(async () => {
-    const auth = new AuthService(null)
-    jest.spyOn(auth, 'validate').mockReturnValue(null)
     await mongod.initialize()
     app = await Test.createTestingModule({
       controllers: [SessionController],
@@ -52,24 +45,6 @@ describe('SessionController', () => {
     }).compile()
 
     sessionController = app.get<SessionController>(SessionController)
-    const sessionData = {
-      store: faker.random.number(),
-      items: [
-        {
-          id: faker.random.number(),
-          title: faker.commerce.productName(),
-          price: parseFloat(faker.commerce.price()),
-          quantity: faker.random.number(),
-          description: faker.commerce.productAdjective(),
-          size: {
-            height: faker.random.number(),
-            length: faker.random.number(),
-            width: faker.random.number()
-          }
-        }
-      ]
-    }
-    sessionValue = plainToClass(SessionDto, sessionData)
   })
 
   beforeEach(async () => {
@@ -82,10 +57,9 @@ describe('SessionController', () => {
 
   describe('createSession', () => {
     it('should create session', async () => {
-      const key = await sessionController.set(sessionValue, {
-        id: faker.random.number(),
-        clientId: faker.random.number()
-      })
+      const item = sessionFactoryDto.createItemDto('food')
+      const sessionValue = sessionFactoryDto.createSessionDto([item])
+      const key = await sessionController.set(sessionValue)
       const resp = await sessionController.get(key)
       expect(sessionValue).toEqual(resp)
     })
@@ -93,6 +67,8 @@ describe('SessionController', () => {
 
   describe('SessionValues', () => {
     it('should accept', async () => {
+      const item = sessionFactoryDto.createItemDto('food')
+      const sessionValue = sessionFactoryDto.createSessionDto([item])
       const status = await validate(sessionValue)
       const size = sessionValue.items[0].size
       expect(size).toBeInstanceOf(SizeDto)
@@ -100,11 +76,12 @@ describe('SessionController', () => {
     })
 
     it('should refuse by store', async () => {
-      const newSession = sessionValue
-      newSession.store = null
+      const item = sessionFactoryDto.createItemDto('food')
+      const sessionValue = sessionFactoryDto.createSessionDto([item])
+      sessionValue.client.clientSecret = ''
       const status = await validate(sessionValue)
       expect(status.length).toBe(1)
-      expect(status[0].constraints.isNotEmpty).toBeTruthy()
+      expect(status[0].children[0].constraints.isNotEmpty).toBeTruthy()
     })
   })
 })

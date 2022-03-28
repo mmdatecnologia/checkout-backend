7bd24282b874fc6b8414ec9094c6d45e90867d5b
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2022-03-28 00:35:06 -0300

    refactor(shopping): shopping creation

--- src/modules/shopping/DTO/shopping.dto.ts
+++ src/modules/shopping/DTO/shopping.dto.ts
@@ -1,17 +1,33 @@
 import { ApiProperty } from '@nestjs/swagger'
+import { Transform } from 'class-transformer'
 import { IsNotEmpty } from 'class-validator'
 
 export class ShoppingDto {
   @ApiProperty()
   @IsNotEmpty()
-  clientId: number
+  clientId: string
+
+  @ApiProperty()
+  @IsNotEmpty()
+  clientSecret: string
 
   @ApiProperty()
   @IsNotEmpty()
   baseUrl: string
+
+  @ApiProperty()
+  @IsNotEmpty()
+  checkoutCallback: string
 }
 
 export class ShoppingDtoResponse extends ShoppingDto {
   @ApiProperty()
-  secretId: string
+  @Transform(({ obj }) => obj.id.toString())
+  id: string
+
+  @ApiProperty()
+  createdAt: Date
+
+  @ApiProperty()
+  updatedAt: Date
 }

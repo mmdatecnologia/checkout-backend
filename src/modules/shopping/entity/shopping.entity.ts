6a1d8cead8d08be7ab669b8e83a74f67e835f4ce
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2022-03-28 00:31:04 -0300

    refactor(shopping): define app base entity

--- src/modules/shopping/entity/shopping.entity.ts
+++ src/modules/shopping/entity/shopping.entity.ts
@@ -1,24 +1,23 @@
 import { AppEntity } from '@checkout/app/app.entity'
 import { ApiProperty } from '@nestjs/swagger'
-import { Exclude } from 'class-transformer'
-import { IsNotEmpty } from 'class-validator'
 import { Column, Entity, Index } from 'typeorm'
 
 @Entity('shopping')
 export class ShoppingEntity extends AppEntity {
   @ApiProperty()
   @Column()
-  @IsNotEmpty()
   @Index()
   clientId: string
 
   @ApiProperty()
   @Column()
-  @IsNotEmpty()
-  @Exclude()
   clientSecret: string
 
   @ApiProperty()
   @Column()
   baseUrl: string
+
+  @ApiProperty()
+  @Column()
+  checkoutCallback: string
 }
